6f12a38a3b67f4f4e9d7cecda0e4b9ea687ab45c
JOY19ANYANGO <joyanyango402@gmail.com>
2025-07-05 14:14:37 +0300

    added Navbar

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -3,19 +3,29 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import PamojaHubOnboarding from './landing-page'
 import PamojaDashboard from './HomeDashboard'
 import ChatPage from './ChatPage.jsx'
+import CommunityPage from './Community'
+import AdminPage from './AdminPage'
+import ResourceSkillPage from './Resourceskill'
+import Navbar from './Navbar'
 import './App.css'
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<PamojaHubOnboarding />} />
-        <Route path="/home" element={<PamojaDashboard />} />
-        <Route path="/chat" element={<ChatPage />} />
-        {/* Add more routes for /circle, /map, /emergency as you build them */}
-      </Routes>
+      <Navbar />
+      <div className="pt-16"> {/* To prevent navbar from overlapping content */}
+        <Routes>
+          <Route path="/" element={<PamojaHubOnboarding />} />
+          <Route path="/home" element={<PamojaDashboard />} />
+          <Route path="/Community" element={<CommunityPage />} />
+          <Route path="/chat" element={<ChatPage />} />
+          <Route path="/AdminPage" element={<AdminPage />} />
+          <Route path="/ResourceSkillPage" element={<ResourceSkillPage />} />
+        </Routes>
+      </div>
     </Router>
   )
 }
 
 export default App
+
